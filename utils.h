e5ed357dbbcabeebc71b6db6f26f4f7bc4032c19
Fred <fredskyward@gmail.com>
2024-06-07 12:10:50 -0700

    macro: added helper macro

--- utils.h
+++ utils.h
@@ -5,3 +5,12 @@
 #define PRINTINT(value) printf(#value": %d\n", value)
 #define PRINTCHAR(value) printf(#value": %c\n", value)
 #define PRINTF(format, ...) printf("("__FILE__" : %d) %s "format"\n", __LINE__, __FUNCTION__, ##__VA_ARGS__)
+#define PRINTARR_INT(array, length) PRINTARR("%d ", array, length)
+#define PRINTARR(format, array, length) \
+{ \
+    int i = 0; \
+    for (int i = 0; i < length; i++) { \
+        printf(format, array[i]); \
+    } \
+    printf("\n"); \
+} 
